a9b1c6d1ada49df74c824cfc4cd89759db7654c8
xinz02 <xinlinchua296@gmail.com>
2024-02-26 16:57:39 +0800

    account overview: retrieve balance and transaction list

--- src/api.ts
+++ src/api.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import healthCheck from '@components/healthcheck/healthCheck.router';
 import user from '@components/user/user.router';
 import transaction from '@components/transaction/transaction.router';
+import account_overview from '@components/account_overview/account_overview.router';
 
 const router: Router = Router();
 router.use(healthCheck);
 router.use(user);
 router.use(transaction);
+router.use(account_overview);
 
 export default router;
---
+++ src/components/account_overview/account_overview.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { retrieveTransactionLists, retrieveBalanceETH } from './account_overview.service';
+
+const getAccountOverview = async (req: Request, res: Response) => {
+  try {
+    const add = req.params.address;
+    if (!add) {
+      res
+        .status(httpStatus.BAD_REQUEST)
+        .json({ success: false, error: 'Missing address parameter' });
+    } else {
+      // Call the service function to retrieve the transaction list
+      const transactionList = await retrieveTransactionLists(add);
+      const balance = await retrieveBalanceETH(add);
+
+      // Send the transaction list as a response
+      res.status(httpStatus.OK).json({ success: true, data: transactionList, balance });
+    }
+  } catch (error) {
+    // Handle errors
+    // console.error('Error in getTransactionList:', error);
+
+    // You can customize the error response based on the error type or use a generic error message
+    res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error: 'Internal Server Error' });
+  }
+};
+
+export default getAccountOverview;
