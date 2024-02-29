3ff7a3ad95a092dab5b6f9a8976edeb7994e6bcc
sunflower2002 <81811162+sunflower2002@users.noreply.github.com>
2024-02-29 12:28:20 +0800

    latesttrxlist

--- src/components/transaction/transaction.router.ts
+++ src/components/transaction/transaction.router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { retrieveTransactions, retrieveTransactionsByHashId } from './transaction.controller';
+import { retrieveTransactions, retrieveTransactionsByHashId ,getLatestTransactionList} from './transaction.controller';
 
 const router: Router = Router();
 
 router.get('/transaction/', retrieveTransactions);
 router.get('/transaction/:hashId', retrieveTransactionsByHashId);
-
+router.get('/transaction/fetch/latestTransactionList', getLatestTransactionList);
 export default router;
