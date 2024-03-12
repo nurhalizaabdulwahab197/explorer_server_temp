452173118f4ecc6e039e1831b05be9023c7f05fd
lk-sam <93167399+lk-sam@users.noreply.github.com>
2024-03-13 01:51:15 +0800

    debug: change search response type

--- src/components/search/search.controller.ts
+++ src/components/search/search.controller.ts
@@ -1,12 +1,14 @@
 // search.controller.ts
 import { Request, Response } from 'express';
+import httpStatus from 'http-status';
 import * as SearchService from './search.service';
 
 const performSearch = async (req: Request, res: Response) => {
   try {
     const { query } = req.params; // Or req.query for query parameters
     const results = await SearchService.search(query);
-    res.json(results);
+    res.status(httpStatus.OK);
+    res.json({ output: results });
   } catch (error) {
     res.status(500).send({ message: 'Error performing search', error });
   }
