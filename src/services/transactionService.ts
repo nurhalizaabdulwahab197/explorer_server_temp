0af0edbd9a5d6a8a3d297d96609ae372c4cda8c9
nurhalizaabdulwahab197 <115357859+nurhalizaabdulwahab197@users.noreply.github.com>
2024-09-26 14:47:57 +0800

    Update transactionService.ts

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -69,7 +69,6 @@ class TransactionService {
 
   async detectAndSaveTransactions(latestBlockNumber?: number) {
     try {
-      // Fetch latest block
       // eslint-disable-next-line no-param-reassign
       if (!latestBlockNumber) latestBlockNumber = Number(await this.web3.eth.getBlockNumber());
       const latestBlock = await this.web3.eth.getBlock(latestBlockNumber, true);
@@ -78,118 +77,118 @@ class TransactionService {
         logger.info('No transactions found in the latest block.');
         return;
       }
-      // Process transactions in the latest block
-      // eslint-disable-next-line consistent-return
+
       const transactionPromises = latestBlock.transactions.map(async (tx) => {
-        if (typeof tx !== 'string') {
-          const transaction = await this.web3.eth.getTransactionReceipt(tx.hash);
-          if (!transaction) {
-            logger.info('No transactions receipt found');
-          }
-          const { baseFeePerGas } = latestBlock;
-          const priorityFeePerGas =
-            Number(tx.maxPriorityFeePerGas) || Number(tx.gasPrice) - Number(baseFeePerGas);
-          // check if the transaction is a contract creation Error checking contract type
-          // to check whether the contract address is null or not. Contract address is null if the item is updated/deleted
-          let receiverAddress: string = tx.to || 'null';
-          if (transaction.contractAddress !== undefined) {
-            receiverAddress = transaction.contractAddress;
-          }
-          // to check whether the address is contract or not
-          const type = await checkAddressType(receiverAddress);
-          let note: any;
-          let onComplete: any;
-
-          if (type === 'contract') {
-            const contractType = await this.checkContractType(receiverAddress);
-            const interactor = new ContractInteractor(abiStorage, methodConfig);
-            const contractProxy = new ContractProxy(interactor, contractType, receiverAddress);
-
-            if (contractType !== 'Unknown') {
-              note = await contractProxy.getHistory();
-              onComplete = await contractProxy.getStatus();
-            } else {
-              note = []; // Assign an empty array
-              onComplete = 'Unknown';
+        try {
+          if (typeof tx !== 'string') {
+            const transaction = await this.web3.eth.getTransactionReceipt(tx.hash);
+            if (!transaction) {
+              logger.info('No transaction receipt found');
+              return;
             }
 
-            // Ensure note is an array before accessing length
-            if (!Array.isArray(note) || note.length <= 0) {
-              note = [{ description: 'No history found for this contract address' }];
+            const { baseFeePerGas } = latestBlock;
+            const priorityFeePerGas =
+              Number(tx.maxPriorityFeePerGas) || Number(tx.gasPrice) - Number(baseFeePerGas);
+
+            let receiverAddress: string = tx.to || 'null';
+            if (transaction.contractAddress !== undefined) {
+              receiverAddress = transaction.contractAddress;
             }
 
-            if (contractType === 'RFID') {
-              const latestNote = note[note.length - 1];
-              const latestDescription = latestNote.description;
+            const type = await checkAddressType(receiverAddress);
+            let note: string;
+            let onComplete: string;
 
-              // Extract the numeric status code from the description
-              const statusCodeMatch = latestDescription.match(/RFID status changed to (\d+)/);
-              if (statusCodeMatch) {
-                const statusCode = parseInt(statusCodeMatch[1], 10);
-                const readableStatus = this.getStatusName(statusCode);
-                note = latestDescription.replace(/\d+$/, readableStatus);
+            if (type === 'contract') {
+              const contractType = await this.checkContractType(receiverAddress);
+              const interactor = new ContractInteractor(abiStorage, methodConfig);
+              const contractProxy = new ContractProxy(interactor, contractType, receiverAddress);
+
+              if (contractType !== 'Unknown') {
+                const history = await contractProxy.getHistory();
+                const status = await contractProxy.getStatus();
+
+                if (contractType === 'RFID') {
+                  const latestNote = history[history.length - 1];
+                  const latestDescription = latestNote.description;
+
+                  const statusCodeMatch = latestDescription.match(/RFID status changed to (\d+)/);
+                  if (statusCodeMatch) {
+                    const statusCode = parseInt(statusCodeMatch[1], 10);
+                    const readableStatus = this.getStatusName(statusCode);
+                    note = latestDescription.replace(/\d+$/, readableStatus);
+                  } else {
+                    note = latestDescription;
+                  }
+                  onComplete = this.getStatusName(Number(status));
+                } else if (contractType === 'CERTIFICATE') {
+                  const latestNote = history[history.length - 1];
+                  note = `${latestNote.action} by ${latestNote.modifiedBy}`;
+                  onComplete = status;
+                } else {
+                  note = 'No history found for this contract address';
+                  onComplete = 'Unknown';
+                }
               } else {
-                note = latestDescription;
+                note = 'Unknown contract type';
+                onComplete = 'Unknown';
               }
-              onComplete = this.getStatusName(Number(onComplete));
-            } else if (contractType === 'CERTIFICATE') {
-              const latestNote = note[note.length - 1];
-              const stringData = `${latestNote.action} by ${latestNote.modifiedBy}`;
-              note = stringData;
             } else {
-              note = 'No history found for this contract address';
-              onComplete = 'Unknown';
+              note = 'Normal transaction';
+              onComplete = 'Completed';
             }
-          } else {
-            note = 'This is a normal transaction';
-            onComplete = 'Success';
-          }
 
-          const transactionData = {
-            hash: tx.hash,
-            block: Number(latestBlock.number),
-            senderAddress: tx.from,
-            amount: Number(this.web3.utils.fromWei(tx.value.toString(), 'ether')),
-            receiverAddress: tx.to || 'null',
-            contractAddress: transaction.contractAddress || 'null',
-            status: transaction.status,
-            input: tx.input || '0x',
-            value: Number(this.web3.utils.fromWei(tx.value.toString(), 'ether')),
-            gasPrice: tx.gasPrice
-              ? Number(this.web3.utils.fromWei(tx.gasPrice.toString(), 'gwei'))
-              : 0,
-            gasLimit: Number(tx.gas),
-            gasUsed: Number(transaction.gasUsed),
-            gasFees: Number(this.web3.utils.fromWei((tx.gasPrice * tx.gas).toString(), 'ether')),
-            timestamp: new Date(Number(latestBlock.timestamp) * 1000),
-            maxFeePerGas: tx.maxFeePerGas
-              ? Number(this.web3.utils.fromWei(tx.maxFeePerGas.toString(), 'gwei'))
-              : 0,
-            maxPriorityFeePerGas: priorityFeePerGas
-              ? Number(this.web3.utils.fromWei(priorityFeePerGas.toString(), 'gwei'))
-              : 0,
-            baseFeePerGas: baseFeePerGas
-              ? Number(this.web3.utils.fromWei(baseFeePerGas.toString(), 'gwei'))
-              : 0,
-            // Calculate the transaction fee based on EIP-1559
-            transactionFee: transaction.effectiveGasPrice
-              ? Number(
-                  this.web3.utils.fromWei(
-                    (
-                      Number(transaction.effectiveGasPrice) * Number(transaction.gasUsed)
-                    ).toString(),
-                    'ether'
+            const transactionData = {
+              hash: tx.hash,
+              block: Number(latestBlock.number),
+              senderAddress: tx.from,
+              amount: Number(this.web3.utils.fromWei(tx.value.toString(), 'ether')),
+              receiverAddress: tx.to || 'null',
+              contractAddress: transaction.contractAddress || 'null',
+              status: transaction.status,
+              input: tx.input || '0x',
+              value: Number(this.web3.utils.fromWei(tx.value.toString(), 'ether')),
+              gasPrice: tx.gasPrice
+                ? Number(this.web3.utils.fromWei(tx.gasPrice.toString(), 'gwei'))
+                : 0,
+              gasLimit: Number(tx.gas),
+              gasUsed: Number(transaction.gasUsed),
+              gasFees: Number(this.web3.utils.fromWei((tx.gasPrice * tx.gas).toString(), 'ether')),
+              timestamp: new Date(Number(latestBlock.timestamp) * 1000),
+              maxFeePerGas: tx.maxFeePerGas
+                ? Number(this.web3.utils.fromWei(tx.maxFeePerGas.toString(), 'gwei'))
+                : 0,
+              maxPriorityFeePerGas: priorityFeePerGas
+                ? Number(this.web3.utils.fromWei(priorityFeePerGas.toString(), 'gwei'))
+                : 0,
+              baseFeePerGas: baseFeePerGas
+                ? Number(this.web3.utils.fromWei(baseFeePerGas.toString(), 'gwei'))
+                : 0,
+              transactionFee: transaction.effectiveGasPrice
+                ? Number(
+                    this.web3.utils.fromWei(
+                      (
+                        Number(transaction.effectiveGasPrice) * Number(transaction.gasUsed)
+                      ).toString(),
+                      'ether'
+                    )
                   )
-                )
-              : 0,
-            note,
-            onComplete,
-          };
-          return TransactionModel.create(transactionData);
+                : 0,
+              note,
+              onComplete,
+            };
+
+            // eslint-disable-next-line consistent-return
+            return await TransactionModel.create(transactionData);
+          }
+        } catch (error) {
+          logger.error(`Error processing transaction ${tx.hash}:`, error);
         }
       });
 
-      await Promise.all(transactionPromises);
+      // eslint-disable-next-line node/no-unsupported-features/es-builtins
+      await Promise.allSettled(transactionPromises);
     } catch (error) {
       logger.error('Error detecting and saving transactions:', error);
     }
