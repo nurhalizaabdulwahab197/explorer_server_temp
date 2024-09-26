b7179fe3464e9eded65f4b6c1f621f8c92b6eddb
lksam <luksamuel57@gmail.com>
2024-09-26 16:16:46 +0800

    Update;: Change to static method

--- src/contracts/abiStorage.ts
+++ src/contracts/abiStorage.ts
@@ -1,33 +1,20 @@
-import fs from 'fs/promises';
-import path from 'path';
+// src/contracts/abiStorage.ts
+
+import CERTIFICATE from '@contracts/contracts/CERTIFICATE.json';
+import RFID from '@contracts/contracts/RFID.json';
 
 class FileAbiStorage {
-  private baseDir: string;
-
-  constructor() {
-    // Ensure baseDir points to 'dist/contracts' after build
-    this.baseDir = path.resolve(__dirname, 'contracts');
-  }
+  private abiMap: { [key: string]: any[] } = {
+    CERTIFICATE: CERTIFICATE.abi,
+    RFID: RFID.abi,
+  };
 
   async getABI(contractType: string): Promise<any[]> {
-    try {
-      // Construct the file path dynamically based on the contract type
-      const filePath = path.join(this.baseDir, `${contractType.toUpperCase()}.json`);
-
-      // Check if the file exists
-      await fs.access(filePath);
-
-      const abiJson = await fs.readFile(filePath, 'utf8');
-      const abiObject = JSON.parse(abiJson);
-
-      // Assuming the JSON structure has an 'abi' key
-      if (!abiObject.abi) {
-        throw new Error('ABI not found in the JSON file');
-      }
-      return abiObject.abi;
-    } catch (error) {
-      throw new Error(`Failed to load ABI from file: ${error.message}`);
+    const abi = this.abiMap[contractType.toUpperCase()];
+    if (!abi) {
+      throw new Error(`ABI not found for contract type: ${contractType}`);
     }
+    return abi;
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
+// webpack.config.js
+
 const path = require('path');
 // eslint-disable-next-line node/no-unpublished-require
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-// eslint-disable-next-line import/no-extraneous-dependencies, node/no-unpublished-require
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/server.ts',
@@ -28,16 +28,4 @@ module.exports = {
     filename: 'server.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'contracts/**/*.json',
-          to: '[path][name][ext]',
-          context: 'src',
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-  ],
 };
